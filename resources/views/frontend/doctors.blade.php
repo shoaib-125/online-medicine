edf05ec06223b1408af34fa21f53cabbd29d4856
shoaib-125 <shoaibyounas980@gmail.com>
2021-07-17 00:32:38 +0500

    doctors layout

--- resources/views/frontend/doctors.blade.php
+++ resources/views/frontend/doctors.blade.php
@@ -7,7 +7,12 @@
             @if(!empty($doctors))
                 @foreach($doctors as $doctor)
                     <div class="col-sm">
+                        <img src="{{$doctor->image}}" alt="Doctor" width="200px" height="300" />
                         <h3>Name: {{$doctor->first_name}} {{$doctor->last_name}}</h3>
+                        <h3>Email: {{$doctor->email}}</h3>
+                        <h3>Phone No: {{$doctor->phone_no}}</h3>
+                        <h3>Specialization: {{$doctor->city}}</h3>
+                        <h3>Address: {{$doctor->address}}</h3>
                     </div>
                 @endforeach
             @endif
